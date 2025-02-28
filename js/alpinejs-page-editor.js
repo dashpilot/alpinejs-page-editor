52e64ae6e8cd9ccf52332961b0946b1381b9e7f0
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2025-02-28 12:00:50 +0100

    fixed an issue when deleting an item and added scrollTo behavior when opening the editor

--- js/alpinejs-page-editor.js
+++ js/alpinejs-page-editor.js
@@ -4,7 +4,7 @@ const html = String.raw;
 editorDiv.innerHTML = html`
     <div class="editor" :class="{ 'open': item !== false }" spellcheck="false" x-cloak>
         <div class="editor-head">
-            <button type="button" class="wdgt-btn-close wdgt-float-end" aria-label="Close" @click="item=false"></button>
+            <button type="button" class="wdgt-btn-close wdgt-float-end" aria-label="Close" @click="closeEditor"></button>
         </div>
         <div class="editor-body">
             <template x-if="item">
@@ -24,7 +24,7 @@ editorDiv.innerHTML = html`
                                         <div>
                                             <span class="wdgt-btn wdgt-btn-outline-secondary btn-file"> Browse <input type="file" @change="(e) => uploadImage(e)" /> </span>
                                             <template x-if="item.image">
-                                                <img :src="item.image" class="wdgt-w-100 wdgt-mt-2" />
+                                                <img :src="item.image" class="wdgt-w-100 wdgt-mt-2 wdgt-img" />
                                             </template>
                                         </div>
                                     </template>
@@ -37,7 +37,7 @@ editorDiv.innerHTML = html`
 
                                     <template x-if="key=='items' && item.items">
                                         <div>
-                                            <template x-for="obj, index in value" :key="index">
+                                            <template x-for="(obj, index) in item.items" :key="index">
                                                 <div class="wdgt-group">
                                                     <div class="wdgt-group-header">
                                                         <span class="wdgt-group-title" x-text="'Item ' + (index + 1)"></span>
@@ -59,7 +59,7 @@ editorDiv.innerHTML = html`
                                                                 <div>
                                                                     <span class="wdgt-btn wdgt-btn-outline-secondary btn-file"> Browse <input type="file" @change="(e) => uploadImage(e, index)" /> </span>
                                                                     <template x-if="item.items[index].image">
-                                                                        <img :src="item.items[index].image" class="wdgt-w-100 wdgt-mt-2" />
+                                                                        <img :src="item.items[index].image" class="wdgt-w-100 wdgt-mt-2 wdgt-img" />
                                                                     </template>
                                                                 </div>
                                                             </template>
@@ -103,12 +103,12 @@ function app() {
             console.log('init');
 
             try {
-                const response = await fetch('data.json'); // Adjust the path if necessary
+                const response = await fetch('data.json');
                 if (!response.ok) {
                     throw new Error('Network response was not ok ' + response.statusText);
                 }
                 const data = await response.json();
-                console.log(data); // Log the data to the console
+                console.log(data);
 
                 this.data = data;
                 this.loaded = true;
@@ -129,6 +129,7 @@ function app() {
                             el.classList.remove('editing');
                         });
                         myel.classList.add('editing');
+                        myel.scrollIntoView({ behavior: 'smooth', block: 'center' });
                     });
                 });
             }, 500);
@@ -137,7 +138,14 @@ function app() {
             console.log(this.data);
             this.item = false;
         },
-        // Array operations
+
+        closeEditor() {
+            this.item = false;
+            document.querySelectorAll('.editing').forEach((el) => {
+                el.classList.remove('editing');
+            });
+        },
+
         addArrayItem() {
             // Clone the structure of the first item
             const newItem = {};
@@ -152,21 +160,36 @@ function app() {
         },
 
         removeArrayItem(index) {
-            if (this.item.items.length == 1) {
-                alert('You can not delete the last item');
+            if (!Array.isArray(this.item.items) || this.item.items.length === 0) {
+                alert('No items to delete');
+                return;
+            }
+
+            if (this.item.items.length === 1) {
+                alert('You cannot delete the last item.');
             } else {
                 if (confirm('Are you sure you wish to delete this item?')) {
-                    this.item.items.splice(index, 1);
+                    // Create a copy of the array
+                    const itemsCopy = [...this.item.items];
+
+                    // Check if the index is valid
+                    if (index >= 0 && index < itemsCopy.length) {
+                        // Remove the item from the copy
+                        itemsCopy.splice(index, 1);
+
+                        // Replace the original array with the modified copy
+                        this.item.items = itemsCopy;
+                    } else {
+                        console.error('Invalid index:', index);
+                    }
                 }
             }
         },
 
         moveArrayItem(index, direction) {
             if (direction === 'up' && index > 0) {
-                // Swap with previous item
                 [this.item.items[index], this.item.items[index - 1]] = [this.item.items[index - 1], this.item.items[index]];
             } else if (direction === 'down' && index < this.item.items.length - 1) {
-                // Swap with next item
                 [this.item.items[index], this.item.items[index + 1]] = [this.item.items[index + 1], this.item.items[index]];
             }
         },
@@ -201,7 +224,6 @@ function app() {
 
                 await mypica.resize(canvas, resizedCanvas);
 
-                // Convert the resized image to a base64 string
                 resizedCanvas.toBlob(
                     (blob) => {
                         const reader = new FileReader();
@@ -211,9 +233,9 @@ function app() {
                             console.log(base64data);
 
                             if (index === null) {
-                                this.item.image = base64data; // Set the base64 string as the value of item[key]
+                                this.item.image = base64data;
                             } else {
-                                this.item.items[index].image = base64data; // Set the base64 string for item[key][index].image
+                                this.item.items[index].image = base64data;
                             }
                         };
                         reader.readAsDataURL(blob);
